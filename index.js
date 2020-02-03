be05f36ea83aec0dcad5cda102e5adfd7ce22a7f
adyprajwal <prajwal.ady@gmail.com>
2020-02-03 15:32:34 +0545

    add onclick tracker

---
+++ index.js
@@ -0,0 +1,15 @@
+//CLICK HANDLER
+
+let idArray = [];
+window.addEventListener("click", function(event){
+    let x = event.target;
+    console.log("id: " + x.id);
+    console.log("classname: " + x.className);
+    console.log("tag: " + x.tagName);
+   
+
+});
+
+
+//SCROLL HANDLER
+
